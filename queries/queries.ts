ed131704b6171062632eedddf858c7804ab7f2cc
shunkominato <shun19871001@gmail.com>
2022-06-25 08:46:27 +0900

    d

--- queries/queries.ts
+++ queries/queries.ts
@@ -10,15 +10,15 @@ export const GET_USERS = gql`
   }
 `
 
-// export const GET_USERS_LOCAL = gql`
-//   query GetUsers {
-//     users(order_by: {create_at: desc}) @client {
-//       id
-//       name
-//       create_at
-//     }
-//   }
-// `
+export const GET_USERS_LOCAL = gql`
+  query GetUsers {
+    users(order_by: {create_at: desc}) @client {
+      id
+      name
+      create_at
+    }
+  }
+`
 
 export const GET_USERSIDS = gql`
   query GetUserIds {
@@ -29,7 +29,7 @@ export const GET_USERSIDS = gql`
 `
 
 export const GET_USERBY_ID = gql`
-  query GetUserById($is: uuid!) {
+  query GetUserById($id: uuid!) {
     users_by_pk(id: $id) {
       id
       name
@@ -59,7 +59,7 @@ export const DELETE_USER = gql`
 `
 
 export const UPDATE_USER = gql`
-  mutation DeleteUser($id: uuid!) {
+  mutation UpdateUser($id: uuid!, $name: String!) {
     update_users_by_pk(pk_columns: {id: $id}, _set: {name: $name}) {
       name
       id
