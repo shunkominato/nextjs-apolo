454d4baf7a51e8a5d11d33164486beab1d89f59d
shunkominato <shun19871001@gmail.com>
2022-06-26 10:19:33 +0900

    f

---
+++ lib/apolloClient.ts
@@ -0,0 +1,25 @@
+import { ApolloClient, HttpLink, InMemoryCache, NormalizedCacheObject } from "@apollo/client";
+
+export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__';
+
+let apolloClient: ApolloClient<NormalizedCacheObject> | undefined;
+
+const createApolloClient = () => {
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    link: new HttpLink({
+      uri: 'https://lenient-wolf-89.hasura.app/v1/graphql',
+    }),
+    cache: new InMemoryCache(),
+  })
+}
+
+export const initializeApollo = (initialState = null) => {
+  const _apolloClient = apolloClient ?? createApolloClient();
+
+  if (typeof window === 'undefined') return _apolloClient
+
+  if(apolloClient) apolloClient = _apolloClient;
+
+  return _apolloClient;
+}
